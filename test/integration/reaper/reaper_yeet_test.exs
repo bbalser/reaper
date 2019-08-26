5afda70a1f7e6dbe169e1ef626dc5414fe4078c6
ManApart <akucera@pillartechnology.com>
2019-08-26 10:01:59 -0400

    Activated tests, local bypass

--- test/integration/reaper/reaper_yeet_test.exs
+++ test/integration/reaper/reaper_yeet_test.exs
@@ -1,68 +1,68 @@
-# defmodule Reaper.YeetTest do
-#   use ExUnit.Case
-#   use Divo
-#   use Tesla
-#   require Logger
-#   alias SmartCity.TestDataGenerator, as: TDG
-#   import SmartCity.TestHelper
-#   import SmartCity.Event, only: [dataset_update: 0]
+defmodule Reaper.YeetTest do
+  use ExUnit.Case
+  use Divo
+  use Tesla
+  require Logger
+  alias SmartCity.TestDataGenerator, as: TDG
+  import SmartCity.TestHelper
+  import SmartCity.Event, only: [dataset_update: 0]
 
-#   @dataset_id "12345-6789"
-#   @endpoints Application.get_env(:reaper, :elsa_brokers)
-#   @success_topic Application.get_env(:reaper, :output_topic_prefix) <> "-" <> @dataset_id
-#   @dlq_topic Application.get_env(:yeet, :topic)
+  @dataset_id "12345-6789"
+  @endpoints Application.get_env(:reaper, :elsa_brokers)
+  @success_topic Application.get_env(:reaper, :output_topic_prefix) <> "-" <> @dataset_id
+  @dlq_topic Application.get_env(:yeet, :topic)
 
-#   @invalid_json_file "includes_invalid.json"
+  @invalid_json_file "includes_invalid.json"
 
-#   @moduletag capture_log: true
+  @moduletag capture_log: true
 
-#   setup_all do
-#     bypass = Bypass.open()
+  setup_all do
+    bypass = Bypass.open()
 
-#     bypass
-#     |> TestUtils.bypass_file(@invalid_json_file)
+    bypass
+    |> TestUtils.bypass_file(@invalid_json_file)
 
-#     Patiently.wait_for!(
-#       fn ->
-#         {type, result} = get("http://localhost:#{bypass.port}/#{@invalid_json_file}")
-#         type == :ok and result.status == 200
-#       end,
-#       dwell: 1000,
-#       max_tries: 20
-#     )
+    Patiently.wait_for!(
+      fn ->
+        {type, result} = get("http://localhost:#{bypass.port}/#{@invalid_json_file}")
+        type == :ok and result.status == 200
+      end,
+      dwell: 1000,
+      max_tries: 20
+    )
 
-#     Elsa.create_topic(@endpoints, @success_topic)
+    Elsa.create_topic(@endpoints, @success_topic)
 
-#     json_dataset =
-#       TDG.create_dataset(%{
-#         id: @dataset_id,
-#         technical: %{
-#           cadence: 1_000,
-#           sourceUrl: "http://localhost:#{bypass.port}/#{@invalid_json_file}",
-#           sourceFormat: "json"
-#         }
-#       })
+    json_dataset =
+      TDG.create_dataset(%{
+        id: @dataset_id,
+        technical: %{
+          cadence: 1_000,
+          sourceUrl: "http://localhost:#{bypass.port}/#{@invalid_json_file}",
+          sourceFormat: "json"
+        }
+      })
 
-#     Brook.Event.send(dataset_update(), :reaper, json_dataset)
+    Brook.Event.send(dataset_update(), :reaper, json_dataset)
 
-#     :ok
-#   end
+    :ok
+  end
 
-#   describe "invalid data" do
-#     test "send failed messages to the DLQ topic" do
-#       eventually(fn ->
-#         messages = TestUtils.get_dlq_messages_from_kafka(@dlq_topic, @endpoints)
+  describe "invalid data" do
+    test "send failed messages to the DLQ topic" do
+      eventually(fn ->
+        messages = TestUtils.get_dlq_messages_from_kafka(@dlq_topic, @endpoints)
 
-#         assert [%{app: "Reaper", dataset_id: @dataset_id} | _] = messages
-#       end)
-#     end
+        assert [%{app: "Reaper", dataset_id: @dataset_id} | _] = messages
+      end)
+    end
 
-#     test "no messages go on to the output topic" do
-#       eventually(fn ->
-#         result = TestUtils.get_data_messages_from_kafka(@success_topic, @endpoints)
+    test "no messages go on to the output topic" do
+      eventually(fn ->
+        result = TestUtils.get_data_messages_from_kafka(@success_topic, @endpoints)
 
-#         assert result == []
-#       end)
-#     end
-#   end
-# end
+        assert result == []
+      end)
+    end
+  end
+end
